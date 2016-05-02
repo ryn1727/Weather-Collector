492e1b23ad07456637030c0ef0012df9c0b8871c
ryn1727 <ryn1727@gmail.com>
2016-05-02 14:43:21 -0400

    Update formatting

--- collector.rb
+++ collector.rb
@@ -19,42 +19,42 @@ loop do
     credentials = eval(File.read("credentials.txt"))
 
     puts "--- Starting Data Collection ---".yellow
-	  #get current data from ryans nest
-	  uri = URI.parse(credentials["nest_firebase_api"])
-	  http = Net::HTTP.new(uri.host, uri.port)
-	  http.use_ssl = true
-	  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-	  response = http.get(uri.request_uri)
-	  thermostat = JSON.parse(response.body)
-	  puts "[Success] - ".green + "Pulled down API data from the Nest Thermostat at: " + Time.now.utc.iso8601
+    #get current data from ryans nest
+    uri = URI.parse(credentials["nest_firebase_api"])
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    response = http.get(uri.request_uri)
+    thermostat = JSON.parse(response.body)
+    puts "[Success] - ".green + "Pulled down API data from the Nest Thermostat at: " + Time.now.utc.iso8601
 
-	  #get current data from the closest weather station to ryans house
-	  uri = URI.parse(credentials["weather_underground_api"])
-	  http = Net::HTTP.new(uri.host, uri.port)
-	  response = http.get(uri.request_uri)
-	  local_weather = JSON.parse(response.body)
-	  puts "[Success] - ".green + "Pulled down API data from the local weather station at: " + Time.now.utc.iso8601
+    #get current data from the closest weather station to ryans house
+    uri = URI.parse(credentials["weather_underground_api"])
+    http = Net::HTTP.new(uri.host, uri.port)
+    response = http.get(uri.request_uri)
+    local_weather = JSON.parse(response.body)
+    puts "[Success] - ".green + "Pulled down API data from the local weather station at: " + Time.now.utc.iso8601
 
-	  #create object data
-	  content_type = "application/json"
+    #create object data
+    content_type = "application/json"
 
-	  #make a hash of both the nest and weather station data
-	  weather_hash = {:thermostat => thermostat, :weather_station => local_weather}
-	  object_name = "api_data_" + Time.now.utc.iso8601
+    #make a hash of both the nest and weather station data
+    weather_hash = {:thermostat => thermostat, :weather_station => local_weather}
+    object_name = "api_data_" + Time.now.utc.iso8601
 
-	  #connect to the object store and get the container
-	  AWS::S3::Base.establish_connection!(:server => credentials["s3endpoint"], :access_key_id => credentials["s3username"], :secret_access_key => credentials["s3password"])
-	  puts "[Success] - ".green + "Created a S3 connection at: " + Time.now.utc.iso8601
+    #connect to the object store and get the container
+    AWS::S3::Base.establish_connection!(:server => credentials["s3endpoint"], :access_key_id => credentials["s3username"], :secret_access_key => credentials["s3password"])
+    puts "[Success] - ".green + "Created a S3 connection at: " + Time.now.utc.iso8601
 
-	  #save ryans data to the object store
-	  S3Object.store(object_name, weather_hash.to_s, credentials["s3bucket"], :content_type => content_type)
-	  puts "[Success] - ".green + "Stored the API data in the S3 object store at: " + Time.now.utc.iso8601
-	  puts "--- Sleeping until the next data collection ---".yellow
+    #save ryans data to the object store
+    S3Object.store(object_name, weather_hash.to_s, credentials["s3bucket"], :content_type => content_type)
+    puts "[Success] - ".green + "Stored the API data in the S3 object store at: " + Time.now.utc.iso8601
+    puts "--- Sleeping until the next data collection ---".yellow
     AWS::S3::Base.disconnect!
     AWS::S3::Service.disconnect!
     AWS::S3::Bucket.disconnect!
     AWS::S3::Service.instance_variable_set(:@buckets, nil)
-	  sleep(interval)
+    sleep(interval)
   rescue Exception => error
     puts "[Error] - ".red + "#{error}".red
     puts "Ending data collection for this interval. Data may or may not have been uploaded to the object store".red
